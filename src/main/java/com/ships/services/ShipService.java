ce4338d1df84d8ef98c3a58747257610b39d46d3
NAQI AHMAD - Student <G00332403@gmit.ie>
2018-03-27 16:40:24 +0100

    show all ships working

---
+++ src/main/java/com/ships/services/ShipService.java
@@ -0,0 +1,23 @@
+package com.ships.services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ships.model.Ship;
+import com.ships.repositories.ShipInterface;
+
+@Service
+public class ShipService {
+	
+	@Autowired
+	private ShipInterface shipInterface;
+	
+	public ArrayList<Ship> getShip(){
+		
+		return (ArrayList<Ship>) shipInterface.findAll();	
+	}
+	
+
+}
