6a1f93e88f0d0f7ce4ddcff0df9e3b9d988b4697
nakster <g00332403@gmit.ie>
2018-04-03 21:34:45 +0100

    added the get request for shipping company and the add page

--- src/main/java/com/ships/controllers/ShippingCompanyController.java
+++ src/main/java/com/ships/controllers/ShippingCompanyController.java
@@ -2,12 +2,16 @@ package com.ships.controllers;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ships.model.Ship;
 import com.ships.model.ShippingCompany;
 import com.ships.services.ShippingCompanyService;
 
@@ -16,13 +20,21 @@ public class ShippingCompanyController {
 
 	@Autowired
 	private ShippingCompanyService shipService;
-	
+
 	@RequestMapping(value = "/showShippingCompanies", method=RequestMethod.GET)
 	public String getShips(Model m){
-		
+
 		ArrayList<ShippingCompany> shippingCompany = shipService.getCompany();
 		m.addAttribute("shippingCompany", shippingCompany);
-		
+
 		return "displayShippingCompany";
 	}
+
+	// this is the get request which directs to the add company page
+	@RequestMapping(value = "/addShippingCompany", method = RequestMethod.GET)
+	public String getAddShip(@ModelAttribute("ship") Ship ship, HttpServletRequest h) {
+
+		return "addShippingCompany";
+	}
+
 }
