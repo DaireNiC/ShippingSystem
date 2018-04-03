037af0992570588b4503bfbf56e47eac005fdb4c
nakster <g00332403@gmit.ie>
2018-04-03 22:06:04 +0100

    added get request and create order jsp page

--- src/main/java/com/ships/controllers/OrderInfoController.java
+++ src/main/java/com/ships/controllers/OrderInfoController.java
@@ -2,13 +2,17 @@ package com.ships.controllers;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ships.model.OrderInfo;
+import com.ships.model.Ship;
 import com.ships.services.OrderInfoService;
 
 @Controller
@@ -16,13 +20,20 @@ public class OrderInfoController {
 
 	@Autowired
 	private OrderInfoService orderInfoService;
-	
+
 	@RequestMapping(value = "/showOrders", method=RequestMethod.GET)
 	public String getShips(Model m){
-		
+
 		ArrayList<OrderInfo> ordersInfo = orderInfoService.getOrders();
 		m.addAttribute("ordersInfo", ordersInfo);
-		
+
 		return "displayOrdersInfo";
+	}
+
+	// this is the get request which directs to the create order page
+	@RequestMapping(value = "/createOrder", method = RequestMethod.GET)
+	public String getAddShip(@ModelAttribute("order") OrderInfo order, HttpServletRequest h) {
+
+		return "createOrder";
 	}
 }
