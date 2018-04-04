e3544d6656890e3bcde2bf7aa764a934941d20d8
nakster <g00332403@gmit.ie>
2018-04-04 14:31:52 +0100

    commments added

--- src/main/java/com/ships/services/OrderInfoService.java
+++ src/main/java/com/ships/services/OrderInfoService.java
@@ -1,6 +1,5 @@
 package com.ships.services;
 
-import java.math.MathContext;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -40,16 +39,19 @@ public class OrderInfoService {
 	}
 	
 	public OrderInfo save(OrderInfo order) {
-		
+		//this sets the date as the current date in the database
 		order.setDate(dateFormat.format(date));	
+		//search the ship and shipping company from the ids 
 		ship =SI.findOne(order.getShip().getSid());
 		shipC = SC.findOne(order.getShippingCompany().getScid());
 		
+		//set the ship scid value to the shipc scid gotten from select  
 		ship.setShippingCompany(shipC);
-
+		//update the balance of the shipping company to the 
+		//new balance = old balance - cost of the ship 
 		shipC.setBalance(shipC.getBalance().subtract(ship.getCost()));
 		
+		//save the changes made 
 		return orderInfoInterface.save(order);
 	}
-
 }
