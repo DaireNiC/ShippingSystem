e38813d4ffa9f6a5f65d8a4efeadfd40ca9f9b00
nakster <g00332403@gmit.ie>
2018-04-12 11:39:44 +0100

    finished the error handling

--- src/main/java/com/ships/controllers/OrderInfoController.java
+++ src/main/java/com/ships/controllers/OrderInfoController.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.persistence.RollbackException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.ships.model.OrderInfo;
 import com.ships.model.Ship;
@@ -84,13 +87,18 @@ public class OrderInfoController {
 			return "redirect:showOrders";
 		}
 	}
-
-	@ExceptionHandler(value = Exception.class)
+	//handles roll back exceptions 
+	@ExceptionHandler(value = RollbackException.class)
+	public String handleErr() {
+		
+		return "Error";
+		
+	}
+	//handles null pointers 
+	@ExceptionHandler(value = NullPointerException.class)
 	public String handleError(HttpServletRequest req, Exception e) {
 
 		return "ErrorPageForOrder";
 	}
-	
-	
 	
 }
